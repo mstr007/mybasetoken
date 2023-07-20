bb7b322afab2c1170d4e9e70380e18ea51ee4eee
mstr007 <116421479+mstr007@users.noreply.github.com>
2023-07-20 09:53:22 +0200

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x0a3133a00d15c1b6493d258ee32f3d598baccae5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
